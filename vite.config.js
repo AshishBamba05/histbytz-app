de61ee60831ff521142527d138e8088976bd488a
Ashish Bamba <ashishbamba@Ashishs-MBP.home>
2025-06-25 03:59:21 -0700

    💣 Nuke and force redeploy

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/eraview-history-app/', // <-- this line is crucial
-  plugins: [react()],
-})
