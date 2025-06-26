17371e47ab1c612f1c7d8a76611cd4791c76a4b8
Ashish Bamba <153144074+AshishBamba05@users.noreply.github.com>
2025-06-26 16:01:49 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import EraView from './EraView.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <EraView />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
